4f0c233ae57e9ccbaaa8c77ad30868a6d38bc188
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-19 16:44:48 +0200

    Change first component template

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // templateUrl: './app.component.html',
+  template: `
+    <h1>Hello World!</h1>
+    <p>This is my first component!</p>
+  `,
 })
 export class AppComponent {
   title = 'first-app';
