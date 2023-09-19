7ce54476a6548128ef9991e4d6f091641f5aca3b
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-19 18:04:44 +0200

    Create class attribute

--- first-app/src/app/my-component/my-component.component.ts
+++ first-app/src/app/my-component/my-component.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "my-component",
     templateUrl: "./my-component.component.html",
     styleUrls: ["./my-component.component.css"]
 })
-export class MyComponent {
+export class MyComponent implements OnInit {
 
+    name = "Angular";
+
+    constructor() { }
+
+    ngOnInit() {
+        console.log("MyComponent initialized!");
+    }
 }
