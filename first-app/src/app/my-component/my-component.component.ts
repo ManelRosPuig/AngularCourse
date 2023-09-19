402c7423a6dfc3c52ca5477ba144ac36cc6eb48e
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-19 18:15:09 +0200

    TypeScript: Strong typed language

--- first-app/src/app/my-component/my-component.component.ts
+++ first-app/src/app/my-component/my-component.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from "@angular/core";
 })
 export class MyComponent implements OnInit {
 
-    name = "Angular";
+    name: string = "Manel";
+    age: number = 20;
 
     constructor() { }
 
