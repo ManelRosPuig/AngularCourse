24775be8489f5c97fb2b585671cf3735096981ae
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-19 16:47:29 +0200

    Change first component styles

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'root',
-  styleUrls: ['./app.component.css'],
+  // styleUrls: ['./app.component.css'],
   // templateUrl: './app.component.html',
   template: `
     <h1>Hello World!</h1>
     <p>This is my first component!</p>
   `,
+  styles: [
+    `
+      h1 {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'first-app';
