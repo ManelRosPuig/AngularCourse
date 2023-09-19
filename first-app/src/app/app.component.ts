5ab6fba93879f90ceecfe1d83a112a868f6dbc5b
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-19 17:21:54 +0200

    Create my_component

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -2,19 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'root',
-  // styleUrls: ['./app.component.css'],
-  // templateUrl: './app.component.html',
-  template: `
-    <h1>Hello World!</h1>
-    <p>This is my first component!</p>
-  `,
-  styles: [
-    `
-      h1 {
-        color: red;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'first-app';
